2d8dbe18f97e44d05d3dfb659e837d233ec0e652
Victor Manuel <victorvpn13@gmail.com>
2021-11-02 14:03:58 -0300

    fix: Reestrutura pastas do projeto

--- sprites.s
+++ sprites.s
@@ -1,8 +1,11 @@
 ### ITENS ###
 .include "sprites/morango.data"
 
+### ORIENTAÇÃO: 0 - VIRADO PRA ESQUERDA, 1 - VIRADO PRA DIREITA
+
 ### MADELINE ###
 .include "sprites/madeline_stop.data"
+.include "sprites/madeline_wall1.data"
 
 ### INIMIGO ###
 .include "sprites/inimigo_stop.data"
